cf999396453f5f8c9f24c3cae080761f5e354d39
HumanBooster <bclerc@humanbooster.com>
2015-03-26 14:39:05 +0100

    CRUD généré pour Article

---
+++ src/HB/BlogBundle/Form/ArticleType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace HB\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('creationDate')
+            ->add('lastEditDate')
+            ->add('publishDate')
+            ->add('published')
+            ->add('enabled')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'HB\BlogBundle\Entity\Article'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'hb_blogbundle_article';
+    }
+}
